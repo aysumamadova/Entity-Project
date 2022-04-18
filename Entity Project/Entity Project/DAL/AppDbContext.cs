98b1db00578d5e5ccfe28becd9f38b957e4fba83
Aysu Memmedova <aysu.memmedova2270@uni.edu.az>
2022-04-18 09:18:13 +0400

    add

---
+++ Entity Project/Entity Project/DAL/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Entity_Project.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entity_Project.DAL
+{
+   public class AppDbContext :DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-5C2LVGI\SQLEXPRESS01;Database=Library2;Trusted_Connection=True;");
+        }
+        //public DbSet<Author> Authors { get; set; }
+        //public DbSet<AuthorBook> AuthorBooks { get; set; }
+        public DbSet<Book> Books { get; set; }
+        //public DbSet<Genre> Genres { get; set; }
+        //public DbSet<GenreBook> GenreBooks { get; set; }
+        public DbSet<Purchase> Purchases { get; set; }
+    }
+}
---
+++ Entity Project/Entity Project/Models/Purchase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entity_Project.Models
+{
+   public class Purchase
+    {
+        public int Id { get; set; }
+        public int BookId { get; set; }
+        public Book Book { get; set; }
+    }
+}
